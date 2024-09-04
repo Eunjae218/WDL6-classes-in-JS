5fc2d1cf48902fb73f5683226e7a550f6ffb0079
Eunjae218 <eunjae.yu5981@gmail.com>
2024-09-04 18:53:39 +0700

    A1 and A2 + class and class inherit and obj and this + make methods

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("katakury")
+
+class student{
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+    age(){
+        let date = new Date();
+        return date.getFullYear() - this.year;
+    }
+
+    
+}
+let student1 = new student("Eunjae", 2022)
+console.log(student1)
+console.log(student1.age())
+
+//A2
+
+class Car{
+    constructor(brand){
+        this.carname = brand
+    }
+
+    carAvailable(){
+        return "I have a " + this.carname;
+    }
+}
+
+class Model extends Car{
+    constructor(brand, model){
+        super(brand)
+        this.model = model
+    }
+
+    showCars(){
+        return this.carAvailable()+"it is a "+this.model;
+    }
+}
+
+let myCar = new Model("Ford", "Mustang")
+console.log(myCar)
+//console.log()
